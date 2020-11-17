7799af37f3c7a83818f89f55eade4f5a71a7b2ee
meganmrichardson <megan.marie.richardson@gmail.com>
2020-11-16 23:44:04 -0800

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, Image, View, Button } from "react-native";
+import logo from "./assets/logo.png";
 
 export default function App() {
+  const [message, setMessage] = useState("Hello Megan!");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.gather}>gather</Text>
+      <Image source={logo} style={styles.logo} />
+      {/* <Text>{message}</Text> */}
+      <Button title="Home" style={styles.button} />
+      <Button title="Profile" style={styles.button} />
+      <Button title="Search" style={styles.button} />
     </View>
   );
 }
@@ -14,8 +20,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  logo: {
+    width: 50,
+    height: 50,
+    marginTop: 10
+    // marginBottom: 20
+  },
+  gather: {
+    color: "#358f35",
+    fontSize: 50
+  },
+  button: {
+    // variant: "outline-dark",
+    size: "lg"
+  }
 });
