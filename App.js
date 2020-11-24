b447fa695f1c32780324aa163ce2d6dbf64a0a24
meganmrichardson <megan.marie.richardson@gmail.com>
2020-11-23 21:24:51 -0800

    current

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 // import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, Image, View, Button } from "react-native";
 import logo from "./assets/logo.png";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Container, Row, Col } from "react-bootstrap";
+import PostList from "./PostList";
 
 const Stack = createStackNavigator();
 
-const App = () => {
+function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode/com/posts")
+      .then(response => response.json())
+      .then(json => setPosts(json));
+  }, []);
+
   return (
     <NavigationContainer>
       {
@@ -19,59 +28,47 @@ const App = () => {
             options={{ title: "gather" }}
           />
           <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="Posts" component={PostScreen} />
         </Stack.Navigator>
       }
     </NavigationContainer>
+
+    // Post List
   );
-};
+}
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <Button
-      title="My Profile"
-      onPress={() => navigation.navigate("Profile", { name: "USER NAME" })}
-    />
+    <Container>
+      <Button
+        title="My Profile"
+        onPress={() => navigation.navigate("Profile", { name: "USER NAME" })}
+      />
+      <Button title="Posts" onPress={() => navigation.navigate("Posts")} />
+    </Container>
   );
 };
 const ProfileScreen = ({ navigation, route }) => {
   return <Text>This is {route.params.name}'s profile</Text>;
 };
+const PostScreen = ({ navigation }) => {
+  return (
+    <Container>
+      <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
+        <Col xs lg="12">
+          <h1 style={{ textAlign: "center" }}>gather f33d</h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
+        <Col xs lg="12">
+          <PostList />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+// const loginScreen = ({ navigation }) => {
+//   return <Container></Container>;
+// };
 
 export default App;
-
-// export default function App() {
-//   const [message, setMessage] = useState("Hello Megan!");
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.gather}>gather</Text>
-//       <Image source={logo} style={styles.logo} />
-//       {/* <Text>{message}</Text> */}
-//       <Button title="Home" style={styles.button} />
-//       <Button title="Profile" style={styles.button} />
-//       <Button title="Search" style={styles.button} />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   },
-//   logo: {
-//     width: 50,
-//     height: 50,
-//     marginTop: 10
-//     // marginBottom: 20
-//   },
-//   gather: {
-//     color: "#358f35",
-//     fontSize: 50
-//   },
-//   button: {
-//     // variant: "outline-dark",
-//     size: "lg"
-//   }
-// });
---
+++ PostList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostList(props) {
+  return <h1>gather f33d</h1>;
+}
